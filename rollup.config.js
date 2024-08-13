279d00319b0220d3b73d01757b3a1dab2ff78cb7
artem <art_nazarov@mail.ru>
2024-08-14 00:25:03 +0300

    Removed umd builds. Added `type: "module"` everywhere.

--- rollup.config.js
+++ rollup.config.js
@@ -6,37 +6,11 @@ import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
 import { terser } from 'rollup-plugin-terser';
 
-const path = require('path');
 const pkg = require('./package.json');
 const ENV_PROD = process.env.BUILD === 'production';
 
 const output = (name) => {
-  const pkg = require(path.join(__dirname, `packages/${name}/package.json`));
-
   return [
-    {
-      file: `packages/${name}/dist/index.umd.js`,
-      format: 'umd',
-      name: 'easepick',
-      sourcemap: false,
-      extend: true,
-      banner: `/**
-* @license
-* Package: ${pkg.name}
-* Version: ${pkg.version}
-* https://easepick.com/
-* Copyright ${(new Date()).getFullYear()} Rinat G.
-* 
-* Licensed under the terms of GNU General Public License Version 2 or later. (http://www.gnu.org/licenses/gpl.html)
-*/`,
-      globals(id) {
-        if (/^@easepick\//.test(id)) {
-          return 'easepick';
-        }
-
-        return id;
-      }
-    },
     {
       file: `packages/${name}/dist/index.esm.js`,
       format: 'esm',
